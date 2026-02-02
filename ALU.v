884eb0e4bae36f323e69a82883398a711ad35b01
22vdc1 <22vdc1@queensu.ca>
2026-02-02 17:14:28 -0500

    Created skeleton for ALU

--- ALU.v
+++ ALU.v
@@ -1,7 +1,70 @@
-module alu(A,B,Result);
+module alu(input wire [31:0] A, 
+				input wire [31:0] B, 
+				input wire [4:0] opcode,  
+				output reg [31:0]  zhi, 
+				output reg [31:0] zlow);
 
-input [31:0] A, B;
-output [31:0] Result;
+wire [31;0] adder_out;				
+RCAdd adder(A, B, adder_out)
+				
+always @(A or B or opcode)
+	begin
+	
+	case (opcode)
+		
+		5b'00000 : zlow = adder_out; //Addition
+			
+		5b'00001 : //Subtraction
+		
+		5b'00010 : zlow = A & B; //and
+		
+		5b'00011 : //or
+		
+		5b'00100 : //Shift right logical
+			
+		5b'00101 : //Shift right arithmetic
+		
+		5b'00110 : //Shift left
+			
+		5b'00111 : //Rotate right
+			
+		5b'01000 : //Rotate left
+		
+		5b'01001 : //Add immediate
+		
+		5b'01010 : //And immediate
+		
+		5b'01011 : //OR immediate
+		
+		5b'01100 : //Division
+			
+		5b'01101 : //Multiplication
+			
+		5b'01110 : //negate
+			
+		5b'01111 : //not
+			
+		
+			
+		
+			
+		
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+						
+		
 
-reg [31:0] Result;
-reg [32:0] LocalCarry;
+
+
+				
+				
+
+
