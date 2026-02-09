57a7338a5f34233991d85d720ed906f9e8b44512
22VDC1 <22vdc1@queensu.ca>
2026-02-09 17:49:32 -0500

    Modify adder module to include Overflow_Flag
    Added Overflow_Flag output to adder module and updated input/output declarations.

--- RCAdd.v
+++ RCAdd.v
@@ -1,10 +1,6 @@
 	//Ripple Carry Adder
-module adder(A, B, Result);
+module adder(input [31:0] A, B, output reg [31:0] Result, Overflow_Flag);
 
-input [31:0] A, B;
-output [31:0] Result;
-
-reg [31:0] Result;
 reg [32:0] LocalCarry;
 
 integer i;
@@ -17,5 +13,15 @@ always@(A or B)
 				Result[i] = A[i]^B[i]^LocalCarry[i];
 				LocalCarry[i+1] = (A[i]&B[i])|(LocalCarry[i]&(A[i]|B[i]));
 		end
+		
+		
+		if (LocalCarry[31] != LocalCarry[32])
+		begin
+			Overflow_Flag = 32'd1;
+		end
+		else begin
+			Overflow_Flag = 32'd0;
+		end
+		
 end
 endmodule
