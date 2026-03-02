8bafb258c786ca0aeced5ee28e61c21c3707a672
22vdc1 <22vdc1@queensu.ca>
2026-03-02 17:22:18 -0500

    Fix missing semicolon

--- Ram.v
+++ Ram.v
@@ -1,14 +1,14 @@
-module RAM (input read, input write, input [8:0] Address, inout [31:0] Data);
+module RAM (input read, input write, input [8:0] Address, inout reg [31:0] Data);
 	
 	reg[31:0] RAM1[0:511];
 				
 	always @(posedge read, posedge write)
 	begin
 		  if (read) begin
-			Data <= RAM1[Address] 
+			Data <= RAM1[Address];
 			end
 			
-		  else if(write)
+		  else if (write)
 		  
 		  begin
 			RAM1[Address] <= Data;
