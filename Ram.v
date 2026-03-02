d97735d5b7aa77a69f0f6355cf343af1cdfb049c
22VDC1 <22vdc1@queensu.ca>
2026-03-02 17:09:05 -0500

    Tailor RAM

--- Ram.v
+++ Ram.v
@@ -1,22 +1,19 @@
-//module RAM6116 (Cs_b, We_b, 0e_b, Address, I0);
-//
-//	input Cs_b;
-//	input We_b;
-//	input 0e_b;
-//	input[7:0] Address;
-//	inout[7:0] I0;
-//	
-//	reg[7:0] RAM1[0:255];
-//	
-//	assign I0 = (Cs_b == 1'b1 | We_b == 1'b1 | 0e_b == 1'b1) ?
-//				8'bZZZZZZZZ : RAM1[Address] ; 
-//				
-//	always @(We_b, Cs_b)
-//	begin
-//	     @(negedge We_b)
-//		  if(Cs_b == 1'b0)
-//		  begin
-//			RAM1[Address] <= I0;
-//		  end
-//		end
-//endmodule
+module RAM (input read, input write, input [8:0] Address, inout [31:0] Data);
+	
+	reg[31:0] RAM1[0:511];
+				
+	always @(posedge read, posedge write)
+	begin
+		  if (read) begin
+			Data <= RAM1[Address] 
+			end
+			
+		  else if(write)
+		  
+		  begin
+			RAM1[Address] <= Data;
+		  
+		  end
+		  
+		end
+endmodule
