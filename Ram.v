e844de6a1a6e05f6e72f0beaadb6bd32a7886dd3
22vdc1 <22vdc1@queensu.ca>
2026-03-16 16:41:56 -0400

    Start testbench for load

--- Ram.v
+++ Ram.v
@@ -4,8 +4,17 @@ module RAM (input clock, input read, input write, input [8:0] Address, input [31
 	
 	reg [31:0] dataReg;
 	
+	initial begin
+		RAM1[0x0] = 32'h0x03800065;
+		RAM1[0x1] = 32'h0x00100072;
+		RAM1[0x2] = 32'h0x0B800065;
+		RAM1[0x3] = 32'h0x08100072;
+		RAM1[0x65] = 32'h0x84;
+		RAM1[0xC9] = 32'h0xC9;
+		end
+	
+	
 	//Must assert read for one clock cycle and get data in MDR on the next clock cycle.
-				
 	always @(posedge clock)
 	begin
 	
---
+++ registerPC.v
@@ -0,0 +1,26 @@
+module registerPC #(parameter DATA_WIDTH_IN = 32, DATA_WIDTH_OUT = 32, INIT = 32'h0)(
+    input clear, clock, enable,
+    input  [DATA_WIDTH_IN-1:0]BusMuxOut,
+    output wire [DATA_WIDTH_OUT-1:0]BusMuxIn
+	 input IncPC
+);
+
+reg [DATA_WIDTH_IN-1:0]q;
+initial q = INIT;
+
+always @ (posedge clock)
+	begin
+		 if (clear) begin
+			  q <= {DATA_WIDTH_IN{1'b0}};
+		 end
+		 else if (enable) begin
+			  q <= BusMuxOut;
+		 end
+		 else if (IncPC)
+			q <= q + 1;
+		end
+	end
+
+assign BusMuxIn = q[DATA_WIDTH_OUT-1:0];
+
+endmodule
