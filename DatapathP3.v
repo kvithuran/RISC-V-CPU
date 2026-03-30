e0ace3b7a6b1c0b8e43f849e8055e7a9c12312e2
22vdc1 <22vdc1@queensu.ca>
2026-03-29 20:16:34 -0400

    Added Tyler's instructions to control unit and began attempting to add control unit to phase 3 datapath

---
+++ DatapathP3.v
@@ -0,0 +1,128 @@
+module DatapathP2(
+    input wire clock
+	output wire CON
+    input wire Reset, Stop, Con_FF
+);
+
+wire clear;
+wire CONin;
+wire HIout, LOout, Zhighout, Zlowout, PCout, MDRout, InPortout, Cout, IRout, MARout, OutPortout;
+wire HIin, LOin, Zhighin, Zlowin, PCin, MDRin, InPortin, Cin, IRin, Yin, MARin, OutPortin;
+wire read, write, IncPC, Grb, Gra, Grc;
+wire Rout, Rin, BAout;
+wire R0in,R1in,R2in,R3in,R4in,R5in,R6in,R7in,R8in,R9in,R10in,R11in,R12in,R13in,R14in,R15in;
+wire R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out;
+
+wire [31:0] Mdatain;
+
+//Outputs from each register to go to bus multiplexer.
+wire [31:0] BusMuxOut;
+wire [31:0] BusMuxInR0;
+wire [31:0] BusMuxInR1;
+wire [31:0] BusMuxInR2;
+wire [31:0] BusMuxInR3;
+wire [31:0] BusMuxInR4;
+wire [31:0] BusMuxInR5;
+wire [31:0] BusMuxInR6;
+wire [31:0] BusMuxInR7;
+wire [31:0] BusMuxInR8;
+wire [31:0] BusMuxInR9;
+wire [31:0] BusMuxInR10;
+wire [31:0] BusMuxInR11;
+wire [31:0] BusMuxInR12;
+wire [31:0] BusMuxInR13;
+wire [31:0] BusMuxInR14;
+wire [31:0] BusMuxInR15;
+wire [31:0] BusMuxInHI;
+wire [31:0] BusMuxInLO;
+wire [31:0] BusMuxInZHI;
+wire [31:0] BusMuxInZLO;
+wire [31:0] BusMuxInPC; 
+wire [31:0] BusMuxInMDR;
+wire [31:0] BusMuxInInPort;
+wire [31:0] BusMuxInCSignExtended;
+wire [31:0] BusMuxInIR;
+wire [31:0] BusMuxInMAR;
+
+wire [31:0] YtoALU;
+
+wire [31:0] zhiresult;
+wire [31:0] zlowresult;
+
+wire [31:0] AddrToMem;
+
+wire [31:0] DataFromRAM;
+
+wire [31:0] Instruction;
+wire [31:0] ToOutputUnit;
+wire [31:0] FromInputUnit;
+
+//The registers themselves. Feed in clear to clear anytime, clock for synchronization, R in to allow register to latch value, BusMuxOut to feed into the register (input to register) and BusMuxIn signals defined above to feed to bus multiplexer.
+registerR0 R0(clear, clock, R0in, BusMuxOut, BusMuxInR0, BAout);
+register R1(clear, clock, R1in, BusMuxOut, BusMuxInR1);
+register R2(clear, clock, R2in, BusMuxOut, BusMuxInR2);
+register R3(clear, clock, R3in, BusMuxOut, BusMuxInR3);
+register R4(clear, clock, R4in, BusMuxOut, BusMuxInR4);
+register R5(clear, clock, R5in, BusMuxOut, BusMuxInR5);
+register R6(clear, clock, R6in, BusMuxOut, BusMuxInR6);
+register R7(clear, clock, R7in, BusMuxOut, BusMuxInR7);
+register R8(clear, clock, R8in, BusMuxOut, BusMuxInR8);
+register R9(clear, clock, R9in, BusMuxOut, BusMuxInR9);
+register R10(clear, clock, R10in, BusMuxOut, BusMuxInR10);
+register R11(clear, clock, R11in, BusMuxOut, BusMuxInR11);
+register R12(clear, clock, R12in, BusMuxOut, BusMuxInR12);
+register R13(clear, clock, R13in, BusMuxOut, BusMuxInR13);
+register R14(clear, clock, R14in, BusMuxOut, BusMuxInR14);
+register R15(clear, clock, R15in, BusMuxOut, BusMuxInR15);
+register HI(clear, clock, HIin, BusMuxOut, BusMuxInHI);
+register LO(clear, clock, LOin, BusMuxOut, BusMuxInLO);
+register Zhigh(clear, clock, Zhighin, zhiresult, BusMuxInZHI);
+register Zlow(clear, clock, Zlowin, zlowresult, BusMuxInZLO);
+registerPC PC(clear, clock, PCin, BusMuxOut, BusMuxInPC, IncPC);
+registerMDR registerMDR(clear, clock, MDRin, read, DataFromRAM, BusMuxOut, BusMuxInMDR);
+register InPort(clear, clock, 1'b1, FromInputUnit, BusMuxInInPort);
+register OutPort(clear, clock, OutPortin, BusMuxOut, ToOutputUnit);
+register IR(clear, clock, IRin, BusMuxOut, BusMuxInIR);
+register Y(clear, clock, Yin, BusMuxOut, YtoALU);
+register MAR(clear, clock, MARin, BusMuxOut, AddrToMem);
+
+RAM ram(clock, read, write, AddrToMem[8:0], BusMuxInMDR, DataFromRAM);
+
+
+//ALU
+
+alu alu(YtoALU, BusMuxOut, BusMuxInIR[31:27], zhiresult, zlowresult);
+
+//CONFF
+con_ff CONFF(
+    .clk(clock),
+    .reset(clear),
+    .CONin(CONin),
+    .bus_data(BusMuxOut),
+    .IR(BusMuxInIR),
+    .CON(CON)
+);
+	
+Bus bus( //Mux
+    BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, BusMuxInR7, BusMuxInR8, BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12, BusMuxInR13, BusMuxInR14, BusMuxInR15, BusMuxInHI, BusMuxInLO, BusMuxInZHI, BusMuxInZLO, BusMuxInPC, BusMuxInMDR, BusMuxInInPort, BusMuxInCSignExtended, BusMuxInIR, BusMuxInMAR,
+    
+	 //Encoder
+    R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, HIout, LOout, Zhighout, Zlowout, PCout, MDRout, InPortout, Cout, IRout, MARout,
+
+    BusMuxOut);
+
+
+	Select_and_Encode Select_and_Encode(BusMuxInIR, Gra, Grb, Grc, Rin, Rout, BAout, Cout, 
+												R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in,
+												R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, BusMuxInCSignExtended);
+
+control_unit control_unit(
+    .clock(clock), .clear(clear), .HIout(HIout), .LOout(LOout), .Zhighout(Zhighout), .Zlowout(Zlowout), .PCout(PCout), .MDRout(MDRout), .InPortout(InPortout), .Cout(Cout), .IRout(IRout), .MARout(MARout), .OutPortout(OutPortout),
+    .HIin(HIin), .LOin(LOin), .Zhighin(Zhighin), .Zlowin(Zlowin), .PCin(PCin), .MDRin(MDRin), .InPortin(InPortin), .Cin(Cin), .IRin(IRin), .Yin(Yin), .MARin(MARin), .OutPortin(OutPortin), .CONin(CONin),
+    .read(read), .write(write), .IncPC(IncPC), .Grb(Grb), .Gra(Gra), .Grc(Grc),
+    .Rout(Rout), .Rin(Rin), .BAout(BAout),
+    .IR(BusMuxInIR)
+    .Reset(Reset), .Stop(Stop), .Con_FF(Con_FF)
+);
+
+endmodule
--- controlunit.v
+++ controlunit.v
@@ -1,19 +1,26 @@
 `timescale 1ns/10ps
 module control_unit (
-	output reg 	Gra, Grb, Grc, Rin, …, Rout, // define the inputs and outputs to your Control Unit here
-					Yin, Zin, PCout, IncPC, MARin,
-					Read, Write, Clear,
-					ADD, AND, SHR,
+	output reg 		clear, HIout, LOout, Zhighout, Zlowout, PCout, MDRout, InPortout, Cout, IRout, MARout, OutPortout,
+					HIin, LOin, Zhighin, Zlowin, PCin, MDRin, InPortin, Cin, IRin, Yin, MARin, OutPortin, CONin,
+					read, write, IncPC, Grb, Gra, Grc,
+					Rout, Rin, BAout,	
 	input 		[31:0] IR,
-	input 		Clock, Reset, Stop, Con_FF);
-	parameter 	reset_state = 4’b0000, fetch0 = 4’b0001, fetch1 = 4’b0010, fetch2 = 4’b0011,
-					add3 = 4’b0100, add4 = 4’b0101, add5 = 4’b0110, …;
-	reg 			[3:0] present_state = reset_state; // adjust the bit pattern based on the number of states
+	input 		clock, Reset, Stop, Con_FF);
+	
+parameter 	reset_state = 8'b00000000, fetch0 = 8'b00000001, fetch1 = 8'b00000010, fetch2 = 8'b00000011,
+				addsub3 = 8'b00000100, addsub4 = 8'b00000101, addsub5 = 8'b00000110, addsub6 = 8'b00000111, addsub7 = 8'b00001000,
+				andor3 = 8'b00001001, andor4 = 8'b00001010, andor5 = 8'b00001011, andor6 = 8'b00001100,
+				load3 = 8'b00001101, load4 = 8'b00001110, load5 = 8'b00001111, load6 = 8'b00010000, load7 = 8'b00010001, load8 = 8'b00010010,
+				loadi3 = 8'b00010011, loadi4 = 8'b00010100, loadi5 = 8'b00010101, loadi6 = 8'b00010110,
+				store3 = 8'b00010111, store4 = 8'b00011000, store5 = 8'b00011001, store6 = 8'b00011010, store7 = 8'b00011011, store8 = 8'b00011100,
+				halt3 = 8'b00011101, fetch1b = 8'b00011110;
+	reg 			[7:0] present_state = reset_state; // adjust the bit pattern based on the number of states
 	
 	
 always @(posedge Clock, posedge Reset) // finite state machine; if clock or reset rising-edge
 	begin
-		if (Reset == 1’b1) present_state = reset_state;
+		if (Reset == 1'b1) present_state = reset_state;
+		else if (stop == 1'b1) present_state = halt3; // define a stop state if needed
 		else case (present_state)
 			reset_state: present_state = fetch0;
 			fetch0: present_state = fetch1;
@@ -22,7 +29,7 @@ always @(posedge Clock, posedge Reset) // finite state machine; if clock or rese
 			fetch2: begin
 
 					case (IR[31:27]) // inst. decoding based on the opcode to set the next state
-						5’b00000: present_state = addsub3; // this is the add instruction Tyler
+						5'b00000: present_state = addsub3; // this is the add instruction Tyler
 						5'b00001: present_state = addsub3; 
 						5'b00010: present_state = andor3;  //Tyler
 						5'b00011: present_state = andor3;  
@@ -62,6 +69,30 @@ always @(posedge Clock, posedge Reset) // finite state machine; if clock or rese
 			//HERE DEFINE WHAT CLOCK CYCLE COMES NEXT. DO FOR ALL INSTRUCTIONS. AFTER LAST CLOCK CYCLE GO TO fetch0.
 			addsub3: present_state = addsub4;
 			addsub4: present_state = addsub5;
+			addsub5: present_state = addsub6;
+			addsub6: present_state = addsub7;
+			addsub7: present_state = fetch0;
+			andor3: present_state = andor4;
+			andor4: present_state = andor5;
+			andor5: present_state = andor6;
+			andor6: present_state = fetch0;
+			load3: present_state = load4;
+			load4: present_state = load5;
+			load5: present_state = load6;
+			load6: present_state = load7;
+			load7: present_state = load8;
+			load8: present_state = fetch0;
+			loadi3: present_state = loadi4;
+			loadi4: present_state = loadi5;
+			loadi5: present_state = loadi6;
+			loadi6: present_state = fetch0;
+			store3: present_state = store4;
+			store4: present_state = store5;
+			store5: present_state = store6;
+			store6: present_state = store7;
+			store7: present_state = store8;
+			store8: present_state = fetch0;
+			halt3: present_state = halt3;
 			//⁞
 		endcase
 	end
@@ -70,9 +101,13 @@ always @(present_state) // do the job for each state : HERE PUT YOUR ACTUAL CLOC
 		case (present_state) // assert the required signals in each state
 			reset_state: begin
 				Gra <= 0; Grb <= 0; Grc <= 0; Yin <= 0; // initialize the signals
-				⁞
+				Zhighin <= 0; Zlowin <= 0; Zhighout <= 0; Zlowout <= 0; PCout <= 0; IncPC <= 0; MARin <= 0;
+				read <= 0; write <= 0; clear <= 1;
+				HIout <= 0; LOout <= 0; InPortout <= 0; Cout <= 0; IRout <= 0; MARout <= 0; OutPortout <= 0;
+				HIin <= 0; LOin <= 0; InPortin <= 0; Cin <= 0; OutPortin <= 0;
 			end
 			fetch0: begin
+					clear <= 0;
 					PCout <= 1; // see if you need to de-assert these signals
 					MARin <= 1;
 					IncPC <= 1;
@@ -93,11 +128,138 @@ always @(present_state) // do the job for each state : HERE PUT YOUR ACTUAL CLOC
 				MDRin <= 0;
 				MDRout <= 1; IRin <= 1;
 			end
-			add3: begin
-					Grb <= 1; Rout <= 1;
-					Yin <= 1;
+			addsub3: begin
+				MDRout <= 0; IRin <= 0;
+				Grb <= 1; Rout <= 1;
+				Yin <= 1;
 			end
-			⁞
+			addsub4: begin
+				Grb <= 0; Rout <= 0; Yin <= 0;
+				Grc <= 1; Rout <= 1;
+				Zhighin <= 1; Zlowin <= 1;
+			end
+			addsub5: begin
+				Grc <= 0; Rout <= 0;
+				Zhighin <= 0; Zlowin <= 0;
+				Zlowout <= 1; Gra <= 1; Rin <= 1;
+			end
+			addsub6: begin
+				Zlowout <= 0; Gra <= 0; Rin <= 0;
+				Zhighout <= 1; HIin <= 1;
+			end
+			addsub7: begin
+				Zhighout <= 0; HIin <= 0;
+			end
+			andor3: begin
+				MDRout <= 0; IRin <= 0;
+				Grb <= 1; Rout <= 1;
+				Yin <= 1;
+			end
+			andor4: begin
+				Grb <= 0; Rout <= 0; Yin <= 0;
+				Grc <= 1; Rout <= 1;
+				Zhighin <= 1; Zlowin <= 1;
+			end
+			andor5: begin
+				Grc <= 0; Rout <= 0;
+				Zhighin <= 0; Zlowin <= 0;
+				Zlowout <= 1; Gra <= 1; Rin <= 1;
+			end
+			andor6: begin
+				Zlowout <= 0; Gra <= 0; Rin <= 0;
+			end
+			load3: begin
+				MDRout <= 0; IRin <= 0;
+				Grb <= 1;
+				BAout <= 1;
+				Rout <= 1;
+				Yin <= 1;
+			end
+			load4: begin
+				Yin <= 0;
+				Grb <= 0;
+				BAout <= 0;
+				Rout <= 0;
+				
+				Cout <= 1;
+				Zlowin <= 1;
+				
+			end
+			load5: begin
+				Cout <= 0; Zhighin <= 0; Zlowin <= 0;
+				Zlowout <= 1; MARin <= 1;
+			end
+			load6: begin
+				Zlowout <= 0; MARin <= 0;
+				read <= 1; MDRin <= 1;
+			end
+			load7: begin
+				read <= 1; MDRin <= 1;
+			end
+			load8: begin
+				read <= 0; MDRin <= 0;
+				MDRout <= 1;  Gra <= 1; Rin <= 1;
+			end
+			loadi3: begin
+				MDRout <= 0; IRin <= 0;
+				Grb <= 1;
+				BAout <= 1;
+				Rout <= 1;
+				Yin <= 1;
+			end
+			loadi4: begin
+				Yin <= 0;
+				Grb <= 0;
+				BAout <= 0;
+				Rout <= 0;
+				
+				Cout <= 1;
+				Zlowin <= 1;
+				
+			end
+			loadi5: begin
+				Cout <= 0; Zhighin <= 0; Zlowin <= 0;
+				Zlowout <= 1; Gra <= 1; Rin <= 1;
+			end
+			loadi6: begin
+				Zlowout <= 0; Gra <= 0; Rin <= 0;
+			end
+			store3: begin
+				MDRout <= 0; IRin <= 0;
+				Grb <= 1;
+				BAout <= 1;
+				Rout <= 1;
+				Yin <= 1;
+			end
+			store4: begin
+				Yin <= 0;
+				Grb <= 0;
+				BAout <= 0;
+				Rout <= 0;
+				
+				Cout <= 1;
+				Zlowin <= 1;
+				
+			end
+			store5: begin
+				Cout <= 0; Zhighin <= 0; Zlowin <= 0;
+				Zlowout <= 1; MARin <= 1;
+			end
+			store6: begin
+				Zlowout <= 0; MARin <= 0;
+				Gra <= 1; Rout <= 1; MDRin <= 1;
+			end
+			store7: begin
+				Gra <= 0; Rout <= 0; MDRin <= 0;
+				write <= 1;
+			end
+			store8: begin
+				write <= 0;
+			end
+			halt3: begin
+				
+			end
+				
 		endcase
 	end
 endmodule
